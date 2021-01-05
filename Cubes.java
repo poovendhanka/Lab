5844bda1bb3e5ce8ea518523c61d535d8b140ebc
Poovendhan K <76984280+poovendhanka@users.noreply.github.com>
2021-01-05 11:06:08 +0530

    Add files via upload

---
+++ Cubes.java
@@ -0,0 +1,25 @@
+package sumofcubes;
+import java.util.*;
+
+public class Cubes {
+    public static int sum(int n)
+    {
+    	int sum=0;
+    	for(int i=1;i<=n;i++)
+    	{
+    		sum+=i*i*i;
+    	}
+    	return sum;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Sum of cubes on n number:"+ sum(n));
+		
+		
+		
+	}
+
+}
